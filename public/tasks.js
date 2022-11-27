db8cd17ba82934ba30854929afdccac3b7cec433
MBMcAuliffe <michael@mbmcauliffe.com>
2022-11-27 16:24:45 -0600

    Functioning tasks post functions

--- public/tasks.js
+++ public/tasks.js
@@ -42,16 +42,43 @@ async function submitPost() {
 
 }
 
-function addPeople () {
+function addPerson () {
 
-	const personId = document.getElementById("peopleSelect").value;
+	const select = document.getElementById("personSelect");
+	const personId = select.value;
 
-	if ( personId == "" ) {
+	if ( personId == "" || task.people.indexOf( personId ) >= 0) {
 		return
 	}
 
 	task.people.push(personId);
+	
+    const fragment = document.createDocumentFragment();
 
-	// Insert Document Fragmentation here
+    const row = fragment.appendChild(document.createElement('div'));
+    row.classList.add("flexRow");
+    row.style.setProperty("justify-content", "flex-start");
+
+    row.appendChild(document.createElement("div"));
+    Array.from(row.children)[row.children.length-1].innerText = select.options[select.selectedIndex].text;
+
+    row.appendChild(document.createElement("div"));
+    Array.from(row.children)[row.children.length-1].classList.add("deleteSymbol");
+    Array.from(row.children)[row.children.length-1].setAttribute("onclick", "removePerson('" + personId + "')");
+
+    document.getElementById("peopleBlock").appendChild(fragment);
+    Array.from(peopleBlock.children)[peopleBlock.children.length-1].id = "fragment" + personId;
+
+    return
+
+}
+
+function removePerson ( personId ) {
+
+	task.people.splice(task.people.indexOf(personId), 1);
+
+	document.getElementById( "fragment" + personId ).remove();
+
+	return
 
 }
